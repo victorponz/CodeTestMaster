568cc44d155fe0630165deadc5c1324c523b3c1e
Víctor Ponz <victorponz@gmail.com>
2024-11-20 11:27:20 +0100

    Aumentar timeout para ejecutar el job

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package database;
+
+public class DatabaseConnection
+{
+    private static DatabaseConnection dbInstance; //Variable para almacenar la unica instancia de la clase
+    private static java.sql.Connection con;
+
+    private DatabaseConnection() {
+        // El Constructor es privado!!
+    }
+
+    public static DatabaseConnection getInstance(){
+        //Si no hay ninguna instancia...
+        if(dbInstance==null){
+            dbInstance= new DatabaseConnection();
+        }
+        return dbInstance;
+    }
+
+    public  static java.sql.Connection getConnection(){
+
+        if(con==null){
+            try {
+                String host = "jdbc:sqlite:src/main/resources/network";
+                con = java.sql.DriverManager.getConnection( host );
+                System.out.println("Conexión realizada");
+            } catch (java.sql.SQLException ex) {
+                System.out.println("Se ha producido un error al conectar: " + ex.getMessage());
+            }
+        }
+
+        return con;
+    }
+}
